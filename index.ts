b923f3a18f9d521a2e30d26fbdf755cd8ae1b246
roopekal <roope.kallio@myy.haaga-helia.fi>
2022-04-11 21:11:32 +0300

    slash commands testing

--- index.ts
+++ index.ts
@@ -10,13 +10,66 @@ const client = new DiscordJS.Client({
 })
 
 client.on("ready", () => {
-   console.log("Bot online");
-});
+   console.log("Bot online")
 
-client.on('messageCreate', (message) => {
-   if (message.content === 'ping'){
-      message.reply({
-         content: 'pong',
+
+const guildId = '221290912964149248'
+const guild = client.guilds.cache.get(guildId)
+let commands
+
+if (guild) {
+   commands = guild.commands 
+}else{
+   commands = client.application?.commands
+}
+commands?.create({
+   name: 'ping',
+   description: 'Replies with pong.',
+})
+commands?.create({
+   name: 'add',
+   description: 'adds 2 numbers',
+   options: [
+      {
+         name: 'num1',
+         description: 'the first number',
+         required: true,
+         type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+      },
+      {
+         name: 'num2',
+         description: 'the second number',
+         required: true,
+         type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+      }
+   ]
+})
+
+})
+
+client.on('interactionCreate', async (interaction) => {
+   if(!interaction.isCommand()){
+      return
+   }
+   const { commandName, options} = interaction
+
+   if (commandName === 'ping') {
+      interaction.reply({
+         content:'pong',
+         ephemeral: true,
+      })
+   } else if (commandName === 'add') {
+      const num1 = options.getNumber('num1')!
+      const num2 = options.getNumber('num2')!
+
+      await interaction.deferReply({
+         ephemeral:true
+      })
+
+      await new Promise(resolve => setTimeout(resolve, 5000))
+
+      interaction.editReply({
+         content: `the number is ${num1+num2}`,          
       })
    }
 })
